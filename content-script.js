46388779a5578579f1ed72468da9cb934b6c0eb1
CHEN Yihui <chenyihui@cyh.ac.cn>
2022-04-21 16:55:23 +0000

    init

---
+++ content-script.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    // 加载main.js
+    const s = document.createElement("script");
+    s.src = chrome.runtime.getURL("main.js");
+    s.onload = function () {
+        s.parentNode.removeChild(s);
+    };
+    (document.head || document.documentElement).appendChild(s);
+})();
